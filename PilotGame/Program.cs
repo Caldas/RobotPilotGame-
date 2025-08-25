3c33b402d1664803da0e7e780e09eb49dad2a40a
Fabio Caldas <fabio.caldas@gmail.com>
2025-08-25 14:04:57 -0300

    Clean up comments by removing unnecessary TODO comments.

--- PilotGame/Program.cs
+++ PilotGame/Program.cs
@@ -7,13 +7,10 @@
 Game game = new Game();
 game.PrintGameStartScreen();
 
-// TODO: Wait for user input any key and them clear the screen so we could draw the game start
-// Expected Output:
 Console.ReadKey(); // Wait for user input
 Console.Clear(); // Clear the console screen
 
-//TODO: Start Game Logic Here
-game.StartGame();
+game.StartGame(); // Start Game Logic Here
 
 /// <summary>
 /// Entry point for the PilotGame application.
